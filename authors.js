0f9705de1d024ddd2c4a357870aafe2185043379
EZ <itsno@me.com>
2019-02-18 13:24:26 -0600

    Update authors.js

--- authors.js
+++ authors.js
@@ -2,7 +2,7 @@ import authorId from './helpers/author-id';
 
 export default authorId([
   // post: false
-  { username: 'corsar_cv', start: '18 Feb 2019', first: '1094964359917457408', post: false },
+  { username: 'corsar_cv', start: '18 Feb 2019', first: '1097565767007170560', post: false },
   { username: 'big_bad_trixter', start: '11 Feb 2019', first: '1094964359917457408' },
 //  { username: 'big_bad_trixter', start: '11 Feb 2019', first: '1094964359917457408' },
 ]);
